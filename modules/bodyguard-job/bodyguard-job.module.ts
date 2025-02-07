b8c20dd8c0f3f45584f7f615323cda30c0336031
Nguyen Gia Hau <nguyengiahau0508>
2025-02-07 09:14:33 +0700

    delete

--- modules/bodyguard-job/bodyguard-job.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BodyguardJobService } from './bodyguard-job.service';
-import { BodyguardJobController } from './bodyguard-job.controller';
-
-@Module({
-  controllers: [BodyguardJobController],
-  providers: [BodyguardJobService],
-})
-export class BodyguardJobModule {}
--- modules/bodyguard-job/dto/update-bodyguard-job.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBodyguardJobDto } from './create-bodyguard-job.dto';
-
-export class UpdateBodyguardJobDto extends PartialType(CreateBodyguardJobDto) {
-  id: number;
-}
